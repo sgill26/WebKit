7630eaabfac11acad027ba3dc70c2d201e26a268
Simon Fraser <simon.fraser@apple.com>
2024-05-14 11:01:51 -0700

    Allow a TiledBackingClient to be a client for multiple TiledBackings rdar://128073464 https://bugs.webkit.org/show_bug.cgi?id=274164
    Reviewed by NOBODY (OOPS!).
    
    AsyncPDFRenderer will need to handle multiple TiledBackings in future, and currently
    there's no way to tell which TiledBacking a client callback is associated with. So
    pass a `TiledBacking&` to all the client callbacks.
    
    Also make TileGrid identifiers unique; previously they were 0 or 1, but that allows
    for confusion between TileGrids belonging to different TiledBackings. So give TileGrid
    a TileGridIdentifier and pass it to the client callbacks.
    
    We also have to add TiledBackingClient::willRemoveGrid() so that clients don't
    accumulate data for TileGrids that have gone away (now that the identifiers are
    monotonically increasing). AsyncPDFRenderer implements this, but it will actually
    never get hit because AsyncPDFRenderer doesn't enable two-level tile caches.
    
    * Source/WebCore/Headers.cmake:
    * Source/WebCore/WebCore.xcodeproj/project.pbxproj:
    * Source/WebCore/platform/graphics/TileGridIdentifier.h: Added.
    * Source/WebCore/platform/graphics/TiledBacking.h:
    * Source/WebCore/platform/graphics/ca/TileController.cpp:
    (WebCore::TileController::setContentsScale):
    (WebCore::TileController::setCoverageRect):
    (WebCore::TileController::willRepaintTile):
    (WebCore::TileController::willRemoveTile):
    (WebCore::TileController::willRepaintAllTiles):
    * Source/WebCore/platform/graphics/ca/TileGrid.cpp:
    (WebCore::TileGrid::TileGrid):
    * Source/WebCore/platform/graphics/ca/TileGrid.h:
    (WebCore::TileGrid::identifier const):
    * Source/WebKit/WebProcess/Plugins/PDF/UnifiedPDF/AsyncPDFRenderer.h:
    (WebKit::TileForGrid::computeHash const):
    (WTF::TileForGridHash::hash):
    (WTF::TileForGridHash::equal):
    (WTF::HashTraits<WebKit::TileForGrid>::emptyValue):
    (WTF::HashTraits<WebKit::TileForGrid>::deletedValue):
    (WebKit::AsyncPDFRenderer::TileRenderInfo::equivalentForPainting const): Deleted.
    (WebKit::AsyncPDFRenderer::TileRenderInfo::equivalentForPaintingIgnoringContentVersion const): Deleted.
    (WebKit::AsyncPDFRenderer::renderInfoForTile): Deleted.
    (WebKit::AsyncPDFRenderer::enqueueTilePaintIfNecessary): Deleted.
    * Source/WebKit/WebProcess/Plugins/PDF/UnifiedPDF/AsyncPDFRenderer.mm:
    (WebKit::AsyncPDFRenderer::willRepaintTile):
    (WebKit::AsyncPDFRenderer::willRemoveTile):
    (WebKit::AsyncPDFRenderer::willRepaintAllTiles):
    (WebKit::AsyncPDFRenderer::coverageRectDidChange):
    (WebKit::AsyncPDFRenderer::tilingScaleFactorDidChange):
    (WebKit::AsyncPDFRenderer::willRemoveGrid):
    (WebKit::operator<<):

--- Source/WebKit/WebProcess/Plugins/PDF/UnifiedPDF/AsyncPDFRenderer.mm
+++ Source/WebKit/WebProcess/Plugins/PDF/UnifiedPDF/AsyncPDFRenderer.mm
@@ -164,9 +164,9 @@ bool AsyncPDFRenderer::renderInfoIsValidForTile(const TileForGrid& tileInfo, con
     return renderInfo.equivalentForPainting(currentRenderInfo);
 }
 
-void AsyncPDFRenderer::willRepaintTile(TileGridIndex gridIndex, TileIndex tileIndex, const FloatRect& tileRect, const FloatRect& tileDirtyRect)
+void AsyncPDFRenderer::willRepaintTile(TiledBacking&, TileGridIdentifier gridIdentifier, TileIndex tileIndex, const FloatRect& tileRect, const FloatRect& tileDirtyRect)
 {
-    auto tileInfo = TileForGrid { gridIndex, tileIndex };
+    auto tileInfo = TileForGrid { gridIdentifier, tileIndex };
 
     auto haveValidTile = [&](const TileForGrid& tileInfo) {
         auto it = m_rendereredTiles.find(tileInfo);
@@ -194,9 +194,9 @@ void AsyncPDFRenderer::willRepaintTile(TileGridIndex gridIndex, TileIndex tileIn
     enqueueTilePaintIfNecessary(tileInfo, tileRect);
 }
 
-void AsyncPDFRenderer::willRemoveTile(TileGridIndex gridIndex, TileIndex tileIndex)
+void AsyncPDFRenderer::willRemoveTile(TiledBacking&, TileGridIdentifier gridIdentifier, TileIndex tileIndex)
 {
-    auto tileInfo = TileForGrid { gridIndex, tileIndex };
+    auto tileInfo = TileForGrid { gridIdentifier, tileIndex };
 
     LOG_WITH_STREAM(PDFAsyncRendering, stream << "AsyncPDFRenderer::willRemoveTile " << tileInfo);
 
@@ -205,12 +205,12 @@ void AsyncPDFRenderer::willRemoveTile(TileGridIndex gridIndex, TileIndex tileInd
     m_rendereredTiles.remove(tileInfo);
 }
 
-void AsyncPDFRenderer::willRepaintAllTiles(TileGridIndex)
+void AsyncPDFRenderer::willRepaintAllTiles(TiledBacking&, TileGridIdentifier)
 {
     clearRequestsAndCachedTiles();
 }
 
-void AsyncPDFRenderer::coverageRectDidChange(const FloatRect& coverageRect)
+void AsyncPDFRenderer::coverageRectDidChange(TiledBacking&, const FloatRect& coverageRect)
 {
     RefPtr plugin = m_plugin.get();
     if (!plugin)
@@ -239,8 +239,28 @@ void AsyncPDFRenderer::coverageRectDidChange(const FloatRect& coverageRect)
         removePreviewForPage(pageIndex);
 }
 
-void AsyncPDFRenderer::tilingScaleFactorDidChange(float)
+void AsyncPDFRenderer::tilingScaleFactorDidChange(TiledBacking&, float)
 {
+}
+
+void AsyncPDFRenderer::willRemoveGrid(WebCore::TiledBacking&, TileGridIdentifier gridIdentifier)
+{
+    m_rendereredTiles.removeIf([gridIdentifier](const auto& keyValuePair) {
+        return keyValuePair.key.gridIdentifier == gridIdentifier;
+    });
+
+    m_currentValidTileRenders.removeIf([gridIdentifier](const auto& keyValuePair) {
+        return keyValuePair.key.gridIdentifier == gridIdentifier;
+    });
+
+    Vector<TileForGrid> requestsToRemove;
+    for (auto& tileRequests : m_requestWorkQueue) {
+        if (tileRequests.gridIdentifier == gridIdentifier)
+            requestsToRemove.append(tileRequests);
+    }
+
+    for (auto& tile : requestsToRemove)
+        m_requestWorkQueue.remove(tile);
 }
 
 void AsyncPDFRenderer::clearRequestsAndCachedTiles()
@@ -653,7 +673,7 @@ void AsyncPDFRenderer::pdfContentChangedInRect(float pageScaleFactor, const Floa
 
 TextStream& operator<<(TextStream& ts, const TileForGrid& tileInfo)
 {
-    ts << "[" << tileInfo.gridIndex << ":" << tileInfo.tileIndex << "]";
+    ts << "[" << tileInfo.gridIdentifier << ":" << tileInfo.tileIndex << "]";
     return ts;
 }
 
